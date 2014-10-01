c4ffc9e99e05568cea389ef991f6df880781bcfe
m4d3 <matthias.ewald@hs-ansbach.de>
2014-10-01 23:07:36 +0200

    Initial Commit

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameLogic : MonoBehaviour {
+
+    public Tile tile;
+    public Transform dropContainer;
+    public Transform berryContainer;
+    public enum Direction {
+        left,
+        right,
+        flip
+    }
+
+    List<Tile> dropTiles = new List<Tile>();
+    List<Tile> stageTiles = new List<Tile>();
+    bool inputReady = false;
+    
+
+    // Use this for initialization
+    void Start() {
+        tile.CreatePool(35);
+        FillDropTiles(5);
+        Drop();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (inputReady) {
+            if (Input.GetButtonDown("Flip")) {
+                RotateStage(Direction.flip);
+            }
+        }
+    }
+
+    void RotateStage(Direction dir) {
+        if (dir == Direction.flip) {
+            LeanTween.rotateZ(berryContainer.gameObject, 180, 1f).setEase(LeanTweenType.easeInOutElastic);
+            foreach (Tile tile in stageTiles) {
+                tile.transform.parent = berryContainer;
+                LeanTween.rotateZ(tile.gameObject, 0, 1f).setEase(LeanTweenType.easeInOutElastic);
+            }
+        }
+    }
+
+    void FillDropTiles(int amount) {
+
+        for (int i = 0; i < amount; i++) {
+            Tile newTile = tile.Spawn(new Vector3(i, 5, 0), Quaternion.identity);
+            newTile.transform.parent = dropContainer;
+            dropTiles.Add(newTile);
+        }
+    }
+
+    void Drop() {
+        foreach (Tile tile in dropTiles) {
+            tile.transform.parent = berryContainer;
+            stageTiles.Add(tile);
+            LeanTween.moveY(tile.gameObject, 0, 1f).setEase(LeanTweenType.easeOutBounce).setOnComplete(DropComplete);
+        }
+        dropTiles.Clear();
+    }
+    
+    void DropComplete() {
+        inputReady = true;
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+    public enum Types {
+        blaubeer,
+        himbeer,
+        stachelbeer
+        
+    }
+    public Types type;
+
+    bool onStage = false;
+    Sprite[] graphics;
+    SpriteRenderer sRenderer;
+
+	// Use this for initialization
+	void Start () {
+        graphics = Resources.LoadAll<Sprite>("Sprites");
+        sRenderer = GetComponent<SpriteRenderer>();
+        SetType();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void SetType() {
+        int randColor = Random.Range(0, 3);
+        if (randColor == 0) {
+            type = Types.blaubeer;
+            sRenderer.sprite = graphics[0];
+        } else if (randColor == 1) {
+            type = Types.himbeer;
+            sRenderer.sprite = graphics[2];
+        } else if (randColor == 2) {
+            type = Types.stachelbeer;
+            sRenderer.sprite = graphics[4];
+        }
+    }
+}
